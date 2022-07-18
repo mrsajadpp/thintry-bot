61c60286eb1788e270007a440fe90588a6a91300
Sajad pp <96676224+mrsajadpp@users.noreply.github.com>
2022-07-18 18:10:29 +0530

    Create qr.js

---
+++ commands/qr.js
@@ -0,0 +1,32 @@
+module.exports.run = (client, message, args) => {
+  const fs = require('fs');
+  const qrUrl = 'http://api.qrserver.com/v1/create-qr-code/?size=150x150&data=';
+  const link = message.content.split('/qr ')[1];
+  if (!link) {
+    message.reply('Please enter a value.');
+  } else {
+    message.reply("Please wait a few second's.");
+    const url = qrUrl + link;
+    qr(url);
+  }
+  function sendImg() {
+    message.reply({ files: ['image/qr.png'] });
+  }
+  function qr(url) {
+    var http = require('http'),
+    Stream = require('stream').Transform,
+    fs = require('fs');
+    http.request(url, function(response) {
+    var data = new Stream();
+    response.on('data', function(chunk) {
+       data.push(chunk);
+    });
+    response.on('end', function() {
+       fs.writeFileSync('image/qr.png', data.read());
+       setTimeout(function() {
+         sendImg();
+       }, 2000);
+    });
+    }).end();
+  }
+}
