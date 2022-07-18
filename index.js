22ad6be9f11c1971af5aec6a3c1945d260566d3e
Sajad pp <96676224+mrsajadpp@users.noreply.github.com>
2022-07-18 18:08:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 
+var fs = require('fs');
+var path = require('path');
+var request = require('request'); 
+var discord = require('discord.js');
+var client = new discord.Client({ intents : ["GUILDS", "GUILD_MESSAGES"] });
+var listen = require('./server.js');
+var prefix = '/';
+client.commands = new discord.Collection();
+var commands = fs.readdirSync("./commands").filter(file => file.endsWith('.js'));
+for(file of commands){
+  const commandName = file.split(".")[0];
+  const command = require(`./commands/${commandName}`);
+  client.commands.set(commandName, command);
+} 
+//Bot online
+client.on('ready', () => {
+  console.log(`${client.user.username} is ready!`);
+     client.user.setActivity("/help", {
+        type: "WATCHING"
+  });
+});
+
+//Message
+client.on('messageCreate', message => {
+  client.user.setActivity("/help", {
+        type: "WATCHING"
+  });
+  if (message.content.startsWith(prefix)) {
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const commandName = args.shift();
+    const command = client.commands.get(commandName);
+    if(!command) return message.reply({ content: "That command doesn't exist!"});
+    command.run(client, message, args)
+  }
+});
+process.on("unhandledRejection", error => console.error("Promise rejection:", error));
+listen();
+client.login('OTc2MzQxMDI3MzY1NDY2MTEy.GX_a1Z.AC0I9gRKpSSlu1rTWyhdH5opYSba8Gy0n5PQ0Y');
