2d77ae282548a6bd83999e6c51a484ae2dc8f00c
Sajad pp <96676224+mrsajadpp@users.noreply.github.com>
2022-07-26 20:01:57 +0530

    Update set.js

--- commands/set.js
+++ commands/set.js
@@ -1,1 +1,10 @@
-
+module.exports.run = (client, message, args, data) => {
+   const msg = message.content.split('/set ')[1];
+        data.push({
+          channel : msg
+       });
+   setTimeout(function () => {
+        var ch = message.guild.channels.cache.get(msg);
+        ch.send('Succesfully added this channel');
+       },1000);
+}
