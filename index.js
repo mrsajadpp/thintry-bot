d5d0a3e0a738ef07f296113e86bffa23caf689b7
Sajad pp <96676224+mrsajadpp@users.noreply.github.com>
2022-07-26 19:20:51 +0530

    update index.js

--- index.js
+++ index.js
@@ -31,8 +31,27 @@ client.on('messageCreate', message => {
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const commandName = args.shift();
     const command = client.commands.get(commandName);
-    if(!command) return message.reply({ content: "That command doesn't exist!"});
-    command.run(client, message, args, data)
+    if (!command) { 
+      message.reply({ content: "That command doesn't exist!"});
+    } else if (message.content.startsWith(prefix+'send')) {
+      for (var l = 0; l < data.length; l++) {
+         var mes = message.content.split('/ ')[1];
+         var chl = message.guild.channels.cache.get(data[l].channel);
+         chl.send(`@everyone New announcement from developer's : ${mes}`);
+      }
+    } else if (message.content.startsWith(prefix+'set')) {
+       const msg = message.content.split('/ ')[1];
+        data.push({
+        channel : msg
+       });
+       for (var i = 0; i < data.length; i++) {
+      if (msg == data[i].channel) {
+        var ch = message.guild.channels.cache.get(data[i].channel);
+        ch.send('Succesfully added this channel');
+      }
+     }
+    }
+    command.run(client, message, args, data);
   }
 });
 process.on("unhandledRejection", error => console.error("Promise rejection:", error));
