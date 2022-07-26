d70a3c3a7338b71577a552cdb8712f5b5aba7272
Sajad pp <96676224+mrsajadpp@users.noreply.github.com>
2022-07-26 18:42:07 +0530

    update alert.js

--- commands/alert.js
+++ commands/alert.js
@@ -1,3 +1,7 @@
 module.exports.run = (client, message, args) => {
-  
+  for (var l = 0; l < data.length; l++) {
+      var mes = message.content.split('- ')[1];
+      var chl = message.guild.channels.cache.get(data[l].channel);
+      chl.send(`@everyone ${mes}`);
+    }
 }
