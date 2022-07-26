1f96f9d74f03b932acc01a84108ddf3bb109555b
Sajad pp <96676224+mrsajadpp@users.noreply.github.com>
2022-07-26 19:21:19 +0530

    Delete alert.js

--- commands/alert.js
+++
@@ -1,7 +0,0 @@
-module.exports.run = (client, message, args, data) => {
-  for (var l = 0; l < data.length; l++) {
-      var mes = message.content.split('/ ')[1];
-      var chl = message.guild.channels.cache.get(data[l].channel);
-      chl.send(`@everyone New announcement from developer's : ${mes}`);
-    }
-}
