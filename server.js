9b418768a179b784f193af83263c69766b6c41d0
Sajad pp <96676224+mrsajadpp@users.noreply.github.com>
2022-07-26 19:43:17 +0530

    update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var router = express.Router();
-var port = process.env.port;
+var port = process.env.port || 3007;
 var screenShot = require('./res/screenshot.js');
 //Web server 
 function listen(){
@@ -12,7 +12,7 @@ function listen(){
 }
 //Routes
 app.get('/', (req, res) => {
-  res.send('home');
+  res.send('Home');
 }); 
 app.get('*', (req, res) => {
   res.send('404');
