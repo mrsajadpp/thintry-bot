486e4225aca4a331b436e7cb87096d77fe4f262e
Sajad pp <96676224+mrsajadpp@users.noreply.github.com>
2022-08-09 10:23:41 +0530

    update screen.js

--- commands/screen.js
+++ commands/screen.js
@@ -17,9 +17,9 @@ function sendImg(){
 }
 function save() {
   if (url.startsWith('http')) {
-    take_screenshot(url, "../images/screenshot.png");
+    take_screenshot(url, "images/screenshot.png");
   } else {
-    take_screenshot('http://'+url, "../images/screenshot.png");
+    take_screenshot('http://'+url, "images/screenshot.png");
   }
   setTimeout(function() {
     sendImg();
