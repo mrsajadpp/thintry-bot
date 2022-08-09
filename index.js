c85d23f6d98b50b6f360256c5d90dfee3e0ec1fe
Sajad pp <96676224+mrsajadpp@users.noreply.github.com>
2022-08-09 10:16:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var discord = require('discord.js');
 var client = new discord.Client({ intents : ["GUILDS", "GUILD_MESSAGES"] });
 var listen = require('./server.js');
 var data = require('./data/data.js');
-let take_screenshot = required('./module/screenshot.js');
+let take_screenshot = require('./module/screenshot.js');
 var prefix = '/';
 client.commands = new discord.Collection();
 var commands = fs.readdirSync("./commands").filter(file => file.endsWith('.js'));
