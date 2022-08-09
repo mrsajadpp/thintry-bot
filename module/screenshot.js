05b990229288415b2f83ce1d21d6956bea76ebd1
Sajad pp <96676224+mrsajadpp@users.noreply.github.com>
2022-08-09 11:00:19 +0530

    update screenshot.js

--- module/screenshot.js
+++ module/screenshot.js
@@ -12,6 +12,10 @@ async function take_screenshot(url, path, sendImg) {
     }
 }).then((response)=>fs.writeFileSync(path, response.data)) // Save the contents of the request (screenshot) to the 'path' file destination
 .catch((e)=>console.log("An error has occured: " + e.message))
-sendImg();
+if (!e.message) {
+  sendImg(path);
+} else {
+  sendImg('image/error.jpg');
+}
 }
 module.exports = take_screenshot;
