76a1e992bf56a84c700ff0ed5f443de313c3bc9c
mrsajadpp <saju4753@gmail.com>
2023-11-23 21:09:17 +0530

    fixes

--- commands/screen.js
+++ commands/screen.js
@@ -39,7 +39,7 @@ module.exports = {
 
     try {
       const browser = await puppeteer.launch({
-        headless: "new",
+        headless: true,
         executablePath: '/usr/bin/google-chrome', // Replace with your Chrome executable path
       });
       const page = await browser.newPage();
